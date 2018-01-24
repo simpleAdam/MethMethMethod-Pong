501cd572d6c3a36ce5497126bee2b76607179ed3
Adam Homer <adamjh1983@gmail.com>
2018-01-24 13:16:19 +0000

    some basic classes and a very simple game loop

--- pong.js
+++ pong.js
@@ -1,16 +1,57 @@
-class Vec {
-    constructor(x=0, y=0) {
+class Vec 
+{
+    constructor(x=0, y=0) 
+    {
         this.x = x;
         this.y = y;
     }
-};
+}
 
+class Rect 
+{
+    constructor(w, h) 
+    {
+        this.pos = new Vec;
+        this.size = new Vec(w,h);
+    }
+}
+
+class Ball extends Rect 
+{
+    constructor() 
+    {
+        super(10,10);
+        this.vel = new Vec;
+    }
+}
 
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
-context.fillStyle = '#000';
-context.fillRect(0,0, canvas.width, canvas.height);
+const ball = new Ball;
+ball.pos.x = 100;
+ball.pos.y = 50;
+ball.vel.x = 100;
+ball.vel.y = 100;
 
-context.fillStyle = '#fff';
-context.fillRect(0,0, 10, 10);
+let lastTime;
+function callback(millis) {
+    if (lastTime) {
+        update((millis - lastTime) / 1000);
+    }
+    lastTime = millis;
+    requestAnimationFrame(callback);
+}
+
+function update(dt) {
+    ball.pos.x += ball.vel.x * dt;
+    ball.pos.y += ball.vel.y * dt;
+    
+    context.fillStyle = '#000';
+    context.fillRect(0,0, canvas.width, canvas.height);
+    
+    context.fillStyle = '#fff';
+    context.fillRect(ball.pos.x,ball.pos.y, ball.size.x, ball.size.y);
+}
+
+callback();
