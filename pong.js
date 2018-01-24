6f56f36c85b8077d63d618f4c5ebb5960db00c80
Adam Homer <adamjh1983@gmail.com>
2018-01-24 13:23:44 +0000

    getters

--- pong.js
+++ pong.js
@@ -14,6 +14,18 @@ class Rect
         this.pos = new Vec;
         this.size = new Vec(w,h);
     }
+    get left() {
+       return this.pos.x - this.size.x/2;
+    }
+    get right() {
+       return this.pos.x + this.size.x/2;
+    }
+    get top() {
+       return this.pos.y - this.size.y/2;
+    }
+    get bottom() {
+       return this.pos.y + this.size.y/2;
+    }
 }
 
 class Ball extends Rect 
@@ -46,6 +58,13 @@ function callback(millis) {
 function update(dt) {
     ball.pos.x += ball.vel.x * dt;
     ball.pos.y += ball.vel.y * dt;
+    
+    if (ball.left<0 || ball.right > canvas.width) {
+        ball.vel.x = - ball.vel.x;
+    }
+    if (ball.top<0 || ball.bottom > canvas.height) {
+        ball.vel.y = - ball.vel.y;
+    }
     
     context.fillStyle = '#000';
     context.fillRect(0,0, canvas.width, canvas.height);
